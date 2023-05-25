fc926694f05de5ad259fc242aa6214cf6e7100a8
shamoon <4887959+shamoon@users.noreply.github.com>
2023-05-25 01:35:19 -0700

    Add unit tests for all REST services

--- src-ui/jest.config.js
+++ src-ui/jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   },
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '/e2e/'],
+  testPathIgnorePatterns: ['/node_modules/', '/e2e/', 'abstract*'],
   transformIgnorePatterns: [`<rootDir>/node_modules/(?!.*\\.mjs$|lodash-es)`],
   moduleNameMapper: {
     '^src/(.*)': '<rootDir>/src/$1',
